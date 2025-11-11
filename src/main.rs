b05445481203360af357ab165953a98f4274f895
Filipe <filipeh291@gmail.com>
2025-11-11 20:04:30 +0000

    Clean mod.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 mod board;
 
 fn main() {
-        let mut board1 = board::board::Board::import_from_file("data/single9x9.txt");
-        board1.display();
-        _ = board1.solve_brute_force();
-        println!("\n\nSolved Board:\n");
-        board1.display();
+    let board1 = board::board::Board::generate_new_sudoku(9, 25);
+    board1.display();
 }
