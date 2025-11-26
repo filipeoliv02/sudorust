4019d7af92b6165e05f4a92c2da74d7954c902c6
Filipe <filipeh291@gmail.com>
2025-11-26 23:54:42 +0000

    Add get number of solutions endpoint

--- src/controller.rs
+++ src/controller.rs
@@ -1,7 +1,7 @@
 use crate::board::Board;
 use axum::response::IntoResponse;
 use axum::{http::StatusCode, Json};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct GenerateBoardPayload {
@@ -20,4 +20,10 @@ pub async fn solve_board(Json(mut payload): Json<Board>) -> impl IntoResponse {
     } else {
         (StatusCode::BAD_REQUEST, Json("No solution found")).into_response()
     }
+}
+
+pub async fn get_number_of_solutions(Json(mut payload): Json<Board>) -> impl IntoResponse {
+    let solutions = payload.get_all_solutions();
+    let number_of_solutions = solutions.len();
+    (StatusCode::OK, Json(number_of_solutions)).into_response()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod board;
 mod controller;
-use crate::controller::{generate_board, solve_board};
+use crate::controller::{generate_board, get_number_of_solutions, solve_board};
 use axum::{routing::post, Router};
 use tracing::info;
 
@@ -11,7 +11,8 @@ async fn main() {
 
     let app = Router::new()
         .route("/generate", post(generate_board))
-        .route("/solve", post(solve_board));
+        .route("/solve", post(solve_board))
+        .route("/number_of_solutions", post(get_number_of_solutions));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     info!("Listening on {}", listener.local_addr().unwrap());
